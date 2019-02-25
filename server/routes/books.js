8b9cca10c6a2275a0a3bd12bd806e3d98f6ece6f
ChrisPTheCoder <ppham13@my.centennialcollege.ca>
2019-02-25 12:48:49 -0500

    Added delete page

--- server/routes/books.js
+++ server/routes/books.js
@@ -106,9 +106,17 @@ router.post('/:id', (req, res, next) => {
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  let id = req.params.id;
+
+  book.remove({_id:id}, (err) =>{
+    if(err){
+      console.log(err);
+      res.end(err);
+    }else{
+      //refresh the page
+      res.redirect('/books');
+    }
+  });
 });
 
 
