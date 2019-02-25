b988403b7867a12032da10efb41147dfc0290ce8
ChrisPTheCoder <ppham13@my.centennialcollege.ca>
2019-02-25 12:39:02 -0500

    Add comment header

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,3 +1,10 @@
+/* 
+  File name: books.js
+  Author's name: Phuong Linh Pham
+  Student ID: 300923800
+  Web App name: My Favourite Books
+*/
+
 // modules required for routing
 let express = require('express');
 let router = express.Router();
