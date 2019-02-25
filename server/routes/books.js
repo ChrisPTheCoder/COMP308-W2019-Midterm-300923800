ad02e4c84d3ae0b5b0a2225a6c8a92667d9c1c0b
ChrisPTheCoder <ppham13@my.centennialcollege.ca>
2019-02-25 13:06:56 -0500

    Added authentication

--- server/routes/books.js
+++ server/routes/books.js
@@ -13,8 +13,18 @@ let mongoose = require('mongoose');
 // define the book model
 let book = require('../models/books');
 
+//function require Authentication
+function requireAuth(req, res, next){
+  //check if the user is logged in
+  console.log(req.user);
+  if(!req.isAuthenticated() || (req.user.username != "admin")){
+    return res.redirect('/login');
+  }
+  next();
+}
+
 /* GET books List page. READ */
-router.get('/', (req, res, next) => {
+router.get('/', requireAuth, (req, res, next) => {
   // find all books in the books collection
   book.find( (err, books) => {
     if (err) {
@@ -31,7 +41,7 @@ router.get('/', (req, res, next) => {
 });
 
 //  GET the Book Details page in order to add a new Book
-router.get('/add', (req, res, next) => {
+router.get('/add', requireAuth, (req, res, next) => {
 
   res.render('books/details',{
     title: 'Add A New Book'
@@ -40,7 +50,7 @@ router.get('/add', (req, res, next) => {
 });
 
 // POST process the Book Details page and create a new Book - CREATE
-router.post('/add', (req, res, next) => {
+router.post('/add', requireAuth, (req, res, next) => {
 
   let newBook = book({
     "Title": req.body.title,
@@ -62,7 +72,7 @@ router.post('/add', (req, res, next) => {
 });
 
 // GET the Book Details page in order to edit an existing Book
-router.get('/:id', (req, res, next) => {
+router.get('/:id', requireAuth, (req, res, next) => {
 
   let id = req.params.id;
   book.findById(id, (err, bookObject) =>{
@@ -80,7 +90,7 @@ router.get('/:id', (req, res, next) => {
 });
 
 // POST - process the information passed from the details form and update the document
-router.post('/:id', (req, res, next) => {
+router.post('/:id', requireAuth, (req, res, next) => {
 
   let id = req.params.id;
   let updatedBook = book({
@@ -104,7 +114,7 @@ router.post('/:id', (req, res, next) => {
 });
 
 // GET - process the delete by user id
-router.get('/delete/:id', (req, res, next) => {
+router.get('/delete/:id', requireAuth, (req, res, next) => {
 
   let id = req.params.id;
 
